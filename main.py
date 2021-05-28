6125f0d5896c7950e7ce36e4cbc4b950b3204474
wilsonmike <michaelwilsonsj@hotmail.com>
2021-05-28 11:44:27 -0400

    logic added for ace in cards | append 1 if over 21

--- main.py
+++ main.py
@@ -22,7 +22,15 @@ for num in range(2):
     computer_cards.append(deal_card())
 
 def calculate_score(cards):
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+        
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+        
     return sum(cards)
+
 
 
 # play game
