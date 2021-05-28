6bbedc3537f0e7b0ca88ea494838b9de306ae960
wilsonmike <michaelwilsonsj@hotmail.com>
2021-05-28 16:17:42 -0400

    added CoffeeMaker class

---
+++ coffee_maker.py
@@ -0,0 +1,17 @@
+class CoffeeMaker:
+    """Models the machine that makes the coffee"""
+    def __init__(self):
+        self.resources = {
+            "water": 300,
+            "milk": 200,
+            "coffee": 100,
+        }
+    
+    def report(self):
+        """Prints a report of all resources."""
+        print(f"Water: {self.resources['water']}ml")
+        print(f"Milk: {self.resources['milk']}ml")
+        print(f"Coffee: {self.resources['coffee']}g")
+    
+    def is_resource_sufficient(self, drink):
+        """Returns true when order can be made, false if ingredients are insufficient"""
