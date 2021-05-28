199fe2cf80067d3ddbf6598e057adf172218255f
wilsonmike <michaelwilsonsj@hotmail.com>
2021-05-28 16:44:17 -0400

    eod commit

--- coffee_menu.py
+++ coffee_menu.py
@@ -8,3 +8,11 @@ class MenuItem:
             "milk": milk,
             "coffee": coffee
         }
+
+class Menu:
+    """Models the Menu with drinks."""
+    def __init__(self):
+        self.menu = [
+            MenuItem(name="latte", water=200, milk=150, coffee=24, cost=2.5),
+            MenuItem(name="", water=200, milk=150, coffee=24, cost=2.5),
+        ]
