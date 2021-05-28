ccd2984be1b017432c5abb1862c593e01a76d9d5
wilsonmike <michaelwilsonsj@hotmail.com>
2021-05-28 16:25:30 -0400

    added make coffee function

--- coffee_maker.py
+++ coffee_maker.py
@@ -15,3 +15,15 @@ class CoffeeMaker:
     
     def is_resource_sufficient(self, drink):
         """Returns true when order can be made, false if ingredients are insufficient"""
+        can_make = True
+        for item in drink.ingredients:
+            if drink.ingredients[item] > self.resources[item]:
+                print(f"Sorry there is not enough {item}.")
+            can_make = False
+        return can_make
+    
+    def make_coffee(self, order):
+        """Deducts the required ingredients from the resources."""
+        for item in order.ingredients:
+            self.resources[item] -= order.ingredients[item]
+        print(f"Here is your {order.name} ☕️. Enjoy!")
