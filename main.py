7298affea520ecec515601d8b321d553596b1163
wilsonmike <michaelwilsonsj@hotmail.com>
2021-05-28 11:53:58 -0400

    added functionality to see if game is over

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ def deal_card():
     return card
 
 def calculate_score(cards):
+    """takes list of cards and returns the score"""
     if sum(cards) == 21 and len(cards) == 2:
         return 0
         
@@ -25,13 +26,17 @@ def calculate_score(cards):
 # user and ai cards
 user_cards = []
 computer_cards = []
+is_game_over = False
 
 for _ in range(2):
     user_cards.append(deal_card())
     computer_cards.append(deal_card())
 
-calculate_score(user_cards)
-calculate_score(computer_cards)
+user_score = calculate_score(user_cards)
+computer_score = calculate_score(computer_cards)
+
+if user_score == 0 or computer_score == 0 or user_score > 21:
+    is_game_over = True
 
 # play game
 print(logo)
