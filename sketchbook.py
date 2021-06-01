a22181fecfc72c276ef65e543204c8b4c04447f6
mike <michaelwilsonsj@hotmail.com>
2021-05-31 21:59:45 -0400

    clear board with c

--- sketchbook.py
+++ sketchbook.py
@@ -15,10 +15,16 @@ def move_left():
 def move_right():
     tim.right(10)
 
+def clear():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
 
 screen.listen()
 screen.onkey(key="w", fun=move_forwards)
 screen.onkey(key="s", fun=move_backwards)
 screen.onkey(key="a", fun=move_left)
 screen.onkey(key="d", fun=move_right)
+screen.onkey(key="c", fun=clear)
 screen.exitonclick()
