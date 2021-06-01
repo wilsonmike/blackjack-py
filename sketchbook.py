d93a848e0f5db20d6b7f6968ba8e46cde54a98aa
mike <michaelwilsonsj@hotmail.com>
2021-05-31 21:49:55 -0400

    functions for different movements

--- sketchbook.py
+++ sketchbook.py
@@ -6,6 +6,16 @@ screen = Screen()
 def move_forwards():
     tim.forward(10)
 
+def move_backwards():
+    tim.backward(10)
+
+def move_left():
+    tim.left(10)
+
+def move_right():
+    tim.right(10)
+    
+
 screen.listen()
 screen.onkey(key="space", fun=move_forwards)
 screen.exitonclick()
