c4e6c9ca3eb38d440d1a55d9d2c89eb2604d3cbb
mike <michaelwilsonsj@hotmail.com>
2021-06-13 18:01:18 -0400

    lopping list

--- hello.py
+++ hello.py
@@ -4,9 +4,15 @@ print(message.lower())
 
 bicycles = ['trek', 'cannondale', 'redline']
 print(bicycles)
+for bike in bicycles:
+    print(bike)
 
 motorcycles = ['honda', 'yamaha', 'suzuki']
 print(motorcycles)
 
 motorcycles[0] = 'ducati'
 print(motorcycles)
+
+for i in range(0,100):
+    if i % 3 == 0 and i % 5 == 0:
+        print('FizzBuzz')
